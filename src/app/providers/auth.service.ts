e46a8cde43e1c4f37c180408eb6693e34e178c8d
Gerald <Tu correo>
2022-07-19 07:27:47 -0500

    Deployment azure

--- src/app/providers/auth.service.ts
+++ src/app/providers/auth.service.ts
@@ -34,7 +34,7 @@ export class AuthService {
   }
 
   login(usuario: any): Observable<any> {
-    const urlEndpoint = 'http://localhost:9100/oauth/token';
+    const urlEndpoint = 'http://20.197.229.7:9100/oauth/token';
 
     const credenciales = btoa('ayllusinchiapp' + ':' + 'code13');
 
